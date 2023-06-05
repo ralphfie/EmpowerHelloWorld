9e1c1c32fa1c7f64ebf93e141610a505cc8caacd
RARAFIE <RARAFIE@MSS-L-RARAFIE2>
2023-06-05 14:35:11 +0800

    Add project files.

---
+++ EmpowerSG_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerSG_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerSG_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("6231B8A0-5A18-432D-A54B-F8E03D265770")]
+[assembly: AssemblyVersion("1.0.0.0")]
